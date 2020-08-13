96eece30e71ee76011c54b9f8d374e90376e94be
Myk Taylor <myk.taylor@gmail.com>
2020-08-13 10:40:59 -0700

    more helpful error messages for bad section_names

--- internal/quickfort/meta.lua
+++ internal/quickfort/meta.lua
@@ -9,10 +9,26 @@ local quickfort_common = reqscript('internal/quickfort/common')
 local log = quickfort_common.log
 local quickfort_command = reqscript('internal/quickfort/command')
 
--- blueprints referenced by meta blueprints must have a label
-local function get_section_name(text, cur_sheet_name)
+-- blueprints referenced by meta blueprints must have a label, even if it's just
+-- a default numeric label. this is so we can provide intelligent error messages
+-- instead of an infinite loop when the blueprint author forgets a slash and
+-- a string is interpreted as a sheet name instead of a label (which is ignored
+-- for .csv files), the label defaults to "1", and the meta blueprint is the
+-- first blueprint in the file.
+local function get_section_name(cell, text, cur_sheet_name)
     local sheet_name, label = quickfort_command.parse_section_name(text)
-    if not label then return nil end
+    if not label then
+        local cell_msg = string.format(
+            'malformed blueprint section name in cell %s', cell)
+        local suggestion = string.format('/%s', text:gsub('/*$', ''))
+        if cur_sheet_name then
+            suggestion = string.format('%s or %s/somelabel',
+                                       suggestion, text:gsub('/*$', ''))
+        end
+        local label_msg = string.format(
+                '(labels are required; did you mean "%s"?)', suggestion)
+        qerror(string.format('%s %s: %s', cell_msg, label_msg, text))
+    end
     if not sheet_name then sheet_name = cur_sheet_name or '' end
     return string.format('%s/%s', sheet_name, label)
 end
@@ -31,13 +47,7 @@ local function do_meta(zlevel, grid, ctx)
     for y, row in pairs(grid) do
         for x, cell_and_text in pairs(row) do
             local cell, text = cell_and_text.cell, cell_and_text.text
-            local section_name = get_section_name(text, ctx.sheet_name)
-            if not section_name then
-                qerror(string.format(
-                        'malformed blueprint section name in cell %s ' ..
-                        '(labels are required; did you mean "/%s"?): "%s"',
-                        cell, text, text))
-            end
+            local section_name = get_section_name(cell, text, ctx.sheet_name)
             table.insert(cells, {y=y, x=x, section_name=section_name})
         end
     end
